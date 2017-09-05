94a110d50797520291cc2ef4fc6e977cf3547a5c
Dawid Poliszak <dawidpoliszak@karpas.pl>
2017-09-05 20:32:11 +0200

    Add root container to react app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-// Set up your application entry point here...
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+<App />,
+ document.getElementById('root'));

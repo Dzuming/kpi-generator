1ffae9fc17425dee62ce3ee0942377260f869768
dpoliszak <dawid.poliszak@lptg.pl>
2017-09-07 16:57:01 +0200

    WIP: Add chart to dashboard

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,11 +1,57 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import ChartistGraph from 'react-chartist';
 
 class Dashboard extends Component {
-  state = {  }
+  state = {}
   render() {
+    let data = {
+      labels: [
+        'W1',
+        'W2',
+        'W3',
+        'W4',
+        'W5',
+        'W6',
+        'W7',
+        'W8',
+        'W9',
+        'W10'
+      ],
+      series: [
+        [
+          1,
+          2,
+          4,
+          8,
+          6,
+          -2,
+          -1,
+          -4,
+          -6,
+          -2
+        ]
+      ]
+    };
+
+    let options = {
+      high: 10,
+      low: -10,
+      axisX: {
+        labelInterpolationFnc: function (value, index) {
+          return index % 2 === 0
+            ? value
+            : null;
+        }
+      }
+    };
+
+    let type = 'Bar';
+
     return (
-      <h1>Dashboard</h1>
-    );
+      <div>
+        <ChartistGraph data={data} options={options} type={type}/>
+      </div>
+    )
   }
 }
 

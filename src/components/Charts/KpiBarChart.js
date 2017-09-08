e1ccdb150a5c15dc204c18f1c0d6bd204a7bb6d3
Dawid Poliszak <dawidpoliszak@karpas.pl>
2017-09-08 21:41:30 +0200

    Remove unnecessary h1 tag from Kpi BarChart.js

--- src/components/Charts/KpiBarChart.js
+++ src/components/Charts/KpiBarChart.js
@@ -3,7 +3,6 @@ import {BarChart} from 'react-d3';
 
 const KpiBarChart = ({data, title, yAxisLabel, xAxisLabel}) => (
     <div>
-        <h1>Dashboard</h1>
         <BarChart
             width={500}
             height={200}

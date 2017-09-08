30290dd4ffff1fb2709cfadee7964fa54a359dbc
Dawid Poliszak <dawidpoliszak@karpas.pl>
2017-09-08 21:44:16 +0200

    Remove unneccesary test

--- src/components/Charts/KpiBarChart.spec.js
+++ src/components/Charts/KpiBarChart.spec.js
@@ -3,12 +3,6 @@ import { shallow } from 'enzyme';
 import KpiBarChart from './KpiBarChart';
 
 describe('<Dashboard />', () => {
-  it('should have a header called \'Dashboard\'', () => {
-    const wrapper = shallow(<KpiBarChart />);
-    const actual = wrapper.find('h1').text();
-    const expected = 'Dashboard';
-    expect(actual).toEqual(expected);
-  });
   it('should have an element called \'barChart\'', () => {
     const wrapper = shallow(<KpiBarChart />);
     const actual = wrapper.find('BarChart').exists();
